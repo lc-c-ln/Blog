c61c618636cb385c2b814be44a9bcb76d3405fc3
PiperChang <goddhqj8000@ajou.ac.kr>
2022-08-07 14:14:15 +0900

    Feat: POST API - PUT, DELETE(완)
    POST API 완. response status, error 관리

--- server/api/post.js
+++ server/api/post.js
@@ -4,31 +4,53 @@ const pool = require("../db/db_connect");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-});
-
-router.get("/:idx", (req, res) => {
-  const idx = req.params.idx;
+  const idx = req.query["id"];
   pool.getConnection((err, connection) => {
-    const sql = `SELECT * from POST where id == ${idx}`;
+    const sql = `SELECT * from POST where (id=${idx})`;
     connection.query(sql, (err, rows) => {
-      res.send(rows);
-      console.log(rows);
+      res.json({ id: `받은 것 : ${idx + rows}+1` });
     });
     connection.release();
   });
 });
 
-// router.post("/", (req, res) => {
-//   const reqData = req.body;
-//   pool.getConnection((err, connection) => {
-//     const sql = `insert into post(title,writer,content,password) values(${
-//       (reqData.title, reqData.writer, reqData.content, reqData.password)
-//     }, `;
+router.post("/", (req, res) => {
+  console.log(req.body);
+  reqData = req.body;
+  const con = pool.getConnection((err, connection) => {
+    const sql = `insert into post(title, writer, content, password) values("${req.body.title}","${req.body.writer}","${req.body.content}","${req.body.password}")`;
+    connection.query(sql, (err, rows) => {
+      res.status(200).send("Post has created")
+    });
+    connection.release();
+  });
+});
 
-//     connection.query(sql, (err, rows) => {
-//       console.log(rows);
-//     });
-//     connection.release();
-//   });
-// });
+router.put("/",(req,res)=>{
+  const con = pool.getConnection((err, connection) => {
+    const sql = `update post set title="${req.body.title}",content="${req.body.content}" where (id=${req.body.id})`
+    connection.query(sql, (err, rows) => {
+      if (err) 
+        res.send(err)
+      else
+        res.status(200).send("Post has edited");
+    });
+    connection.release();
+  });
+})
+
+
+router.delete("/",(req,res)=>{
+  const con = pool.getConnection((err, connection) => {
+    const sql = `delete from post where (id=${req.body.id})`
+    connection.query(sql, (err, rows) => {
+      if (err) 
+        res.send(err)
+      else
+        res.status(200).send("Post has deleted");
+    });
+    connection.release();
+  });
+})
+
 module.exports = router;
