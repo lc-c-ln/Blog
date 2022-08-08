0c3d50bfefa53fbdca2ff8642b2ea56f9224c468
PiperChang <goddhqj8000@ajou.ac.kr>
2022-08-08 20:52:10 +0900

    Feat: PostList page prev,next 버튼 추가
    prev, next 버튼 추가 및 button 값 제대로 안나오는 문제 수정

--- client/src/page/PostList/PostList.tsx
+++ client/src/page/PostList/PostList.tsx
@@ -2,12 +2,9 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function PostList() {
-  // 전체 게시물 개수 가져오기
-  // 페이지 바뀌기 누를 때 마다, 전체 페이지 수 가져오고, 밑에 있는 버튼들
-  // post들 가져오기
-  const [currentPage, setCurrentPage] = useState(1); // 현재 페이지
+  const [currentPage, setCurrentPage] = useState(1); 
   const [totalPageNum, setTotalPageNum] = useState(0);
-  const [postList, setPostList] = useState([]); // 현재 페이지의 post들의 정보를 가진 data
+  const [postList, setPostList] = useState([]); 
 
   useEffect(() => {
     axios
@@ -17,27 +14,26 @@ export default function PostList() {
         },
       })
       .then((res) => {
-        setTotalPageNum(Math.floor(res.data.cnt[0].cnt / 10) + 1);
+        setTotalPageNum(Math.floor((res.data.cnt[0].cnt - 1) / 10) + 1);
         setPostList(res.data.page);
       });
   }, [currentPage]);
 
   const posts = postList.map((post) => {
-    console.log(post);
     return (
-      <li>
+      <li key={post["id"]}>
         {post["title"]}
-        {post["writer"]}        
+        {post["writer"]}
         {post["reg_date"]}
-        댓글 수: {post["comment_cnt"]} 
-        조회 수: {post["view_cnt"]} 
-        좋아요 수: {post["like_cnt"]} 
+        댓글 수: {post["comment_cnt"]}
+        조회 수: {post["view_cnt"]}
+        좋아요 수: {post["like_cnt"]}
       </li>
     );
   });
 
   const pageButtons = [...Array(10)].map((num, idx) => {
-    const page = Math.floor((currentPage-1) / 10) + idx + 1; 
+    const page = Math.floor(currentPage / 10) * 10 + idx + 1;
     return (
       <li
         key={page}
@@ -46,7 +42,6 @@ export default function PostList() {
         }}
         onClick={() => {
           setCurrentPage(page);
-          console.log(page);
         }}
       >
         {page}
@@ -60,8 +55,32 @@ export default function PostList() {
       <div>
         {process.env.REACT_APP_API_SERVER_URL}
         <ul>{posts}</ul>
+        {currentPage}
       </div>
-      <ul>{pageButtons}</ul>
+      <ul>
+        <li
+          onClick={() => {
+            setCurrentPage(Math.floor(currentPage / 10) * 10 - 1);
+          }}
+          style={{ display: currentPage > 10 ? "inline-block" : "none" }}
+        >
+          Prev
+        </li>
+        {pageButtons}
+        <li
+          onClick={() => {
+            setCurrentPage(Math.floor(currentPage / 10) * 10 + 11);
+          }}
+          style={{
+            display:
+              Math.floor(currentPage / 10) * 10 + 9 <= totalPageNum
+                ? "inline-block"
+                : "none",
+          }}
+        >
+          Next
+        </li>
+      </ul>
     </main>
   );
 }
