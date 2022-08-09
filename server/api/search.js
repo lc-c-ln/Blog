d0c84bc48bb1e59501fb2e25760c5e9216e23a9f
PiperChang <goddhqj8000@ajou.ac.kr>
2022-08-10 01:48:51 +0900

    Feat: Search API 수정
    Search API totalPageCnt 제대로 안나오는 버그 수정

--- server/api/search.js
+++ server/api/search.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get("/", (req, res) => {
   const category = req.query["category"]; //title,writer,hashtag,content
   const keyword = req.query["keyword"];
-  const page = req.query["page"]
+  const page = req.query["page"];
   const con = pool.getConnection((err, connection) => {
     const sql =
       category == "hashtag"
@@ -15,16 +15,22 @@ router.get("/", (req, res) => {
         where (id in (
             select post_id 
             from post_tag 
-            where (tag_id in (select id from tag where (name like '%${keyword}%'))))) LIMIT 10 OFFSET ${(page-1)*10};
+            where (tag_id in (select id from tag where (name like '%${keyword}%')))));
         `
-        : `select id, title, writer, reg_date, comment_cnt, view_cnt, like_cnt from post where ${category} like '%${keyword}%' LIMIT 10 OFFSET ${(page-1)*10}`;
-        connection.query(sql, (err, rows) => {
-          if (err) 
-            res.send(err);
-          else {
-            res.json({posts:rows, cnt:rows.length})
-          }
-        })
+        : `select id, title, writer, reg_date, comment_cnt, view_cnt, like_cnt from post where ${category} like '%${keyword}%'`;
+    connection.query(sql, (err, rows) => {
+      if (err) res.send(err);
+      else {
+        res.json({
+          posts: rows.slice((page - 1) * 10, page * 10),
+          totalPageCnt:
+            Math.floor((rows.length - 1) / 10) + 1 == 0
+              ? 1
+              : Math.floor((rows.length - 1) / 10) + 1,
+        });
+      }
+    });
+
     connection.release();
   });
 });
