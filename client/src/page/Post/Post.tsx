ba291b3b82db215501b3f959b10d5f8adfeeacb4
PiperChang <goddhqj8000@ajou.ac.kr>
2022-08-10 04:13:12 +0900

    Feat: Post Page Postdata setting
    Post get API 이용해서 Data 가져오기
    State 깔끔하게 정의하는 법 필요
    Related to: #17

--- client/src/page/Post/Post.tsx
+++ client/src/page/Post/Post.tsx
@@ -1,6 +1,13 @@
 import axios from "axios";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+
+interface PostData {
+  title:string,
+  writer:string,
+  reg_date:Date,
+  content:string
+}
 
 export default function Post() {
   // post_id
@@ -14,13 +21,38 @@ export default function Post() {
   //     console.log(res);
   //   })
   // }
-  axios.get(`//${process.env.REACT_APP_API_SERVER_URL}/post`,{
-    params:{
-      post_id: param.post_id
-    }
-  }).then((res)=>{
-    console.log(res.data);
-  })
+  const [postData, setPostData] = useState({
+    title:"",
+    writer:"",
+    reg_date:"",
+    content:"",
+    comment_cnt:0,
+    view_cnt:0,
+    like_cnt:0
+  });
 
-  return <div>param</div>;
+  useEffect(() => {
+    axios
+      .get(`//${process.env.REACT_APP_API_SERVER_URL}/post`, {
+        params: {
+          post_id: param.post_id,
+        },
+      })
+      .then((res) => {
+        setPostData({
+          ...res.data,
+        });
+        console.log(postData);        
+      });
+  },[]);
+
+  return (
+    <div>
+      <h1>{postData.title}</h1>
+      <div>
+        {postData.content}
+        {postData.reg_date}
+        </div>      
+    </div>
+  );
 }
