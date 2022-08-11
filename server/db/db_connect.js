e4be5953eb0bccbf6087394424cb290afeb049ef
PiperChang <goddhqj8000@ajou.ac.kr>
2022-08-12 02:24:00 +0900

    Chore: npm 명령어 수정

--- server/db/db_connect.js
+++ server/db/db_connect.js
@@ -1,16 +1,15 @@
-const mysql = require("mysql2")
-const config = require('../config/config')
+const mysql = require("mysql2");
+const config = require("../config/config");
+require("dotenv").config();
 
-console.log(process.env.NODE_ENV);
 const pool = mysql.createPool({
-    ...config[process.env.NODE_ENV],
-    connectionLimit:20,
+  ...config[process.env.NODE_ENV],
+  connectionLimit: 20,
 });
 
-pool.getConnection(function(err){
-    if(err) throw err;
-    else
-    console.log('Connencted');
-})
+pool.getConnection(function (err) {
+  if (err) throw err;
+  else console.log("Connencted");
+});
 
-module.exports = pool
+module.exports = pool;
