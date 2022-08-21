0d2de81f1d00e31b7f3a51d39ba968a3d1f369cb
KCHamp <goddhqj8000@ajou.ac.kr>
2022-08-21 14:26:01 +0900

    Fix: production config db 관련 정보 노출되지 않게 수정

---
+++ server/config/config.js
@@ -0,0 +1,16 @@
+module.export = {
+  development: {
+    user: "root",
+    password: "0000",
+    database: "nts",
+    multipleStatements : true,
+    host: "127.0.0.1"
+  },
+  production: {
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database,
+    multipleStatements : true,
+    host: process.env.host
+  }
+}
